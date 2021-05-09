8af513bbe5ce5d7aa22348de69b9936b890fe30e
Nusaniz <67667654+nusaniz@users.noreply.github.com>
2021-05-09 09:16:06 +0700

    Create password page.js

---
+++ password page.js
@@ -0,0 +1,18 @@
+<script language="JavaScript">
+  
+//   variabel
+var password;
+var pass1="jagoankodekeren";
+password=prompt('Masukkan password untuk membuka halaman ini!','');
+
+// cek password
+if (password==pass1) alert('Password Benar! Klik OK untuk membuka halaman ini!'); 
+else {
+  window.location="https://google.com";
+}
+
+</script>
+
+
+referensi:
+  https://www.jagoankode.com/cara-membuat-password-di-halaman-statis-blogger/
